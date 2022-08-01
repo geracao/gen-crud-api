73484057159626db3bfe4547832cee74b070d0d8
GabrielLBl <gabrielleitecardoso123@gmail.com>
2022-08-01 14:36:08 -0300

    Adicao final de CORS para API

--- API/Program.cs
+++ API/Program.cs
@@ -4,12 +4,24 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.AllowAnyHeader()
+                                 .AllowAnyMethod()
+                                 .AllowAnyOrigin();
+                      });
+});
+
+builder.Services.AddControllers();
 
 builder.Services.AddDbContext<MembroContext>(options =>
 {
@@ -28,6 +40,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
